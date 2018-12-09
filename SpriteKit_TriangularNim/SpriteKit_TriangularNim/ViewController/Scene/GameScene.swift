57774e12b312157102e0fbc1437d5a0b6406e065
Yu-Bin Weng <linuxice0609@gmail.com>
2018-12-09 18:19:14 +0800

    感謝 Kevin Wang (圓終於可以正常動作了)

--- SpriteKit_TriangularNim/SpriteKit_TriangularNim/ViewController/Scene/GameScene.swift
+++ SpriteKit_TriangularNim/SpriteKit_TriangularNim/ViewController/Scene/GameScene.swift
@@ -19,7 +19,8 @@ class GameScene: SKScene {
     
     typealias LinePoint = (first: CGPoint?, next: CGPoint?)
     typealias LinearEquationParameter = (a: CGFloat, b: CGFloat)
-
+    typealias IntersectionInCircle = (p1: CGPoint, p2: CGPoint)
+    
     let NodeName = "Nim"
     let ImageName = (normal: "TouchNormal", selected: "TouchSelected")
     
@@ -85,6 +86,17 @@ extension GameScene {
         addChild(shapeNode!)
     }
     
+    /// 測試上面的點有沒有被連到
+    private func nimTest() {
+        enumerateChildNodes(withName: NodeName) { (node, _) in
+            self.touchedPointNode(node, point: self.linePoint)
+        }
+    }
+}
+
+// MARK: - 小工具
+extension GameScene {
+    
     /// 畫線的路徑
     private func drawLinePath() -> UIBezierPath? {
         
@@ -105,46 +117,77 @@ extension GameScene {
         
         return bezierPath
     }
-}
-
-// MARK: - 小工具
-extension GameScene {
     
-    /// 測試上面的點有沒有被連到
-    private func nimTest() {
-        
-        enumerateChildNodes(withName: NodeName) { (node, _) in
-            self.touchedPoint(with: node)
-        }
-    }
-    
-    /// 求出直線方程式 (y = ax + b) 與圓型方程式 ((x-x0)^2 + (y-y0)^2 = r^2) 的聯立解 => (a^2 + 1)x^2 + 2(a*b - a*y0 - x0)x + (x0^2 + y0^2 + b^2 - r^2 - 2*b*y0)
-    private func touchedPoint(with node: SKNode) {
+    /// 求出直線方程式 (y = ax + b) 與圓型方程式 ((x-x0)^2 + (y-y0)^2 = r^2) 的聯立解
+    /// (a^2 + 1)x^2 + 2(a*b - a*y0 - x0)x + (x0^2 + y0^2 + b^2 - r^2 - 2*b*y0)
+    /// 感謝 Kevin-Wang
+    private func touchedPointNode(_ node: SKNode, point: LinePoint) {
         
         guard let myNode = node as? SKSpriteNode,
-              let linearEquationParameter = findLinearEquationParameter(point: self.linePoint),
+              let linearEquationParameter = findLinearEquationParameter(point: point),
               let radius = Optional.some(myNode.frame.width / 2.0),
               let nodeCenter = Optional.some(CGPoint(x: myNode.frame.midX, y: myNode.frame.midY))
         else {
             return
         }
         
-        let r = radius
         let (a, b) = linearEquationParameter
-        let (x0, y0) = (x0: nodeCenter.x, y0: nodeCenter.y)
-        let factor = findFactor(a: a, b: b, x0: x0, y0: y0, r: r)
-        let nodeImageName = (factor < 0) ? ImageName.normal : ImageName.selected
         
-        myNode.texture = SKTexture(imageNamed: nodeImageName)
+        firstNodeInCircle(myNode, a: a, b: b, center: nodeCenter, r: radius)
+        otherNodeInCircle(myNode, point: point, a: a, b: b, center: nodeCenter, r: radius)
+    }
+}
+
+extension GameScene {
+    
+    /// 求該點是否在該圓的範圍之內 (該點跟與該圓心的圓心距離 <= 半徑)
+    private func pointInCircleRect(_ point: CGPoint? ,center: CGPoint, radius: CGFloat) -> Bool {
+        
+        guard let linePoint = point else { return false }
+        
+        let (x0, y0, r) = (center.x, center.y, radius)
+        let (x1, y1) = (linePoint.x, linePoint.y)
+        let isInCircle = (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) <= r * r
+        
+        return isInCircle
     }
     
-    /// 求出二方一次方程式的因子 (b^b - 4*a*c)
+    /// 求取兩點間的線段長度 P0(x0, y0) -> P1(x1, y1)
+    private func lineLength(p0: CGPoint, p1: CGPoint) -> CGFloat {
+        let length = sqrt((p0.x - p1.x) * (p0.x - p1.x) + (p0.y - p1.y) * (p0.y - p1.y))
+        return length
+    }
+    
+    
+    /// 求出直線在該圓上的交點 (x = -B ± √(B^2 - 4AC) / 2A & y = ax + b)
+    private func findIntersectionOnCircle(a: CGFloat, b: CGFloat, center: CGPoint, r: CGFloat) -> IntersectionInCircle? {
+        
+        let (x0, y0) = (center.x, center.y)
+        
+        let A = a * a + 1
+        let B = 2 * (a * b - a * y0 - x0)
+        let C = x0 * x0 + y0 * y0 + b * b - r * r - 2 * b * y0
+        let factor = B * B - 4 * A * C
+        
+        if factor < 0 { return nil }
+        
+        let x1 = (-B + sqrt(factor)) / (2 * A)
+        let x2 = (-B - sqrt(factor)) / (2 * A)
+        let y1 = a * x1 + b
+        let y2 = a * x2 + b
+        
+        let (p1, p2) = (CGPoint(x: x1, y: y1), CGPoint(x: x2, y: y2))
+        let intersectionInCircle: IntersectionInCircle = (p1, p2)
+        
+        return intersectionInCircle
+    }
+    
+    /// 求出二方一次方程式的因子 (b^2 - 4*a*c)
     private func findFactor(a: CGFloat, b: CGFloat, x0: CGFloat, y0: CGFloat, r: CGFloat) -> CGFloat {
         
         let A = a * a + 1
         let B = 2 * (a * b - a * y0 - x0)
         let C = x0 * x0 + y0 * y0 + b * b - r * r - 2 * b * y0
-        
         let factor = B * B - 4 * A * C
         
         return factor
@@ -163,5 +206,41 @@ extension GameScene {
         let b = nextLinePoint.y - a * nextLinePoint.x
         
         return LinearEquationParameter(a: a, b: b)
+    }
+    
+    /// 一開始的點是否在圓內的處理
+    private func firstNodeInCircle(_ node: SKSpriteNode, a: CGFloat, b: CGFloat, center: CGPoint, r: CGFloat) {
+        
+        let isInCircle = pointInCircleRect(linePoint.first, center: center, radius: r)
+        let nodeImageName = isInCircle ? ImageName.selected : ImageName.normal
+        
+        node.texture = SKTexture(imageNamed: nodeImageName)
+    }
+    
+    /// 其它的點是否在圓內的處理
+    private func otherNodeInCircle(_ node: SKSpriteNode, point: LinePoint, a: CGFloat, b: CGFloat, center: CGPoint, r: CGFloat) {
+        
+        guard let firstLinePoint = point.first,
+              let nextLinePoint = point.next,
+              let intersection = findIntersectionOnCircle(a: a, b: b, center: center, r: r)
+        else {
+            return
+        }
+        
+        let lengthForLine = lineLength(p0: firstLinePoint, p1: nextLinePoint)
+        let lengthP1 = lineLength(p0: firstLinePoint, p1: intersection.p1)
+        let lengthP2 = lineLength(p0: firstLinePoint, p1: intersection.p2)
+        
+        let (x1, x2) = (firstLinePoint.x < nextLinePoint.x) ? (firstLinePoint.x, nextLinePoint.x) : (nextLinePoint.x, firstLinePoint.x)
+        let (y1, y2) = (firstLinePoint.y < nextLinePoint.y) ? (firstLinePoint.y, nextLinePoint.y) : (nextLinePoint.y, firstLinePoint.y)
+        
+        let minLength = min(lengthP1, lengthP2)
+        let minPoint = (lengthP1 > lengthP2) ? intersection.p2 : intersection.p1
+        
+        let isRightDirection = (x1...x2 ~= minPoint.x) || (y1...y2 ~= minPoint.y)
+        
+        if ((lengthForLine >= minLength) && isRightDirection) {
+            node.texture = SKTexture(imageNamed: ImageName.selected)
+        }
     }
 }
